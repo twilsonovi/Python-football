9ab374d10b10c768aad90165e9a5f41da0efa9cb
twilsonovi <48108113+twilsonovi@users.noreply.github.com>
2019-03-06 21:20:32 +0000

    Add files via upload

---
+++ result algorithm.py
@@ -0,0 +1,18 @@
+import random
+#Define dictionaries for starting line-ups here:
+
+
+#Calculate sum of modifiers here:
+
+#Define total score function here: greater overall score, calculated by random number added to total modifier,
+
+#Define result function here -
+# if difference between scores = <10, draw, if >10, positive result. prints 'Team X or Y won!'
+
+#Define scoreline function here - winning team goals = losing team goals + random number OR if draw then both random no.
+
+#Define scorer here - for every goal, use a random number (weighted towards attackers to define scorer. Print scorers
+#underneath the scoreline
+
+#add points gained to running total, and goals to scorer running totals
+
